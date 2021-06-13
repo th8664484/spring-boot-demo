ac04cfc3b21884c2ae703806bfa794840bc36b7e
th8664484 <th1950241409@163.com>
2021-06-13 20:26:04 +0800

    01 demo Holler Word

---
+++ demo-01/src/main/java/com/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    /**
+     * @RequestMapping
+     * 指定方法和请求之间的映射
+     */
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello TH";
+    }
+}
