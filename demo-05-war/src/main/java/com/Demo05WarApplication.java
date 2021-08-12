d22375f0b4480a303fdbc8216318cb32815ce5cc
th8664484 <th1950241409@163.com>
2021-08-12 14:49:13 +0800

    05 打war包

---
+++ demo-05-war/src/main/java/com/Demo05WarApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo05WarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo05WarApplication.class, args);
+    }
+
+}
