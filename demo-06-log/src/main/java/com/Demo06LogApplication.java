90a34824e90597c2548d4ac35c5eab2b3cda36d3
th8664484 <th1950241409@163.com>
2021-08-13 16:50:38 +0800

    06 日志

---
+++ demo-06-log/src/main/java/com/Demo06LogApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo06LogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo06LogApplication.class, args);
+    }
+
+}
